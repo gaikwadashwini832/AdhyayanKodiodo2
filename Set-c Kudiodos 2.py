4d03b8f82aa8b25d24e88dbaf77f5af3dc60fd88
gaikwadashwini832 <gaikwadashwini832@gmail.com>
2024-09-20 12:19:19 +0530

    1st commit

---
+++ Set-c Kudiodos 2.py
@@ -0,0 +1,36 @@
+ch=int(input("Enter the vowels:"))
+if ch==0:
+    s1=input("Enter the string:",s1)
+    print("a")
+    
+elif ch==1:
+     s2=int(input("Enter the string:",e))
+     print("e")
+    
+elif ch==2:
+     s3=int(input("Enter the string:",i))
+     print("i")
+    
+elif ch==3:
+     s4=int(input("Enter the string:",0))
+     print("o")
+    
+elif ch==4:
+     s5=int(input("Enter the string:",u))
+     print("u")
+else:
+    no_of_vowel=s1+s2+s3+s4+s5
+    Total=(no_of_vowel/5)
+    print("Total")
+
+string=output
+
+
+
+
+
+
+
+
+
+
---
+++ Set-c kudiodo.py
@@ -0,0 +1,8 @@
+Marks=int(input("Enter the marks:"))
+if Marks>100:
+    print("InvalidMarksException:Marks must be from 0 to 100")
+elif Marks<0:
+    print("InvalidMarksException:Marks must be from 0 to 100")
+else:
+    print("You got 90 marks!")
+    
